edbca7e28c8045be6638ec790883a96e8fbf5c62
kfox-ama <kayla.m.fox@ama-inc.com>
2016-08-26 15:22:55 -0400

    Addition of Apache 2.0 License headers

--- labs/dc_mosaicker.py
+++ labs/dc_mosaicker.py
@@ -1,3 +1,30 @@
+# Copyright 2016 United States Government as represented by the Administrator 
+# of the National Aeronautics and Space Administration. All Rights Reserved.
+#
+# Portion of this code is Copyright Geoscience Australia, Licensed under the 
+# Apache License, Version 2.0 (the "License"); you may not use this file 
+# except in compliance with the License. You may obtain a copy of the License 
+# at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# The CEOS 2 platform is licensed under the Apache License, Version 2.0 (the 
+# "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at 
+# http://www.apache.org/licenses/LICENSE-2.0. 
+# 
+# Unless required by applicable law or agreed to in writing, software 
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT 
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+# License for the specific language governing permissions and limitations 
+# under the License.
+
+
+# Author: KMF
+# Creation date: 2016-06-14
+# Modified by:
+# Last modified date: 
+
 import numpy as np
 import xarray as xr
 
@@ -10,9 +37,6 @@ import os
 import collections
 import gdal
 from datetime import datetime
-
-# Author: KMF
-# Creation date: 2016-06-14
 
 def create_mosaic(dataset_in, clean_mask=None, no_data=-9999):
     """
